ec3cf41b262474ab665febf37406399daaac750a
mrwuyunfeng <913821317@qq.com>
2017-09-24 10:18:11 +0800

    wyf0924

---
+++ src/test/testwyf/WyfTest.java
@@ -0,0 +1,11 @@
+package testwyf;
+
+/**
+ * Created by Administrator on 2017/9/24 0024.
+ * Author 武云峰
+ */
+public class WyfTest {
+    public static void main(String[] args) {
+        System.out.println("1234");
+    }
+}
